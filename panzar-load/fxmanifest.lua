e2cf8089f2fb9b47a2a50fbb814355b76aa6526d
ItsWrath <34563489+ItsWrath@users.noreply.github.com>
2021-02-11 23:57:57 -0800

    Add files via upload

---
+++ panzar-load/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version "bodacious"
+
+game "gta5"
+
+files {
+    -- Main
+    'edit_this.html',
+    'keks.css',
+ -- 'bg1.png', Background Image
+    
+    -- Musiken
+    'music/music.mp3'
+}
+
+loadscreen 'edit_this.html'
