478ab8a38b4e06b343400e8247bbfc6b426694b4
ItsWrath <34563489+ItsWrath@users.noreply.github.com>
2021-02-12 00:15:24 -0800

    Update fxmanifest.lua

--- panzar-load/fxmanifest.lua
+++ panzar-load/fxmanifest.lua
@@ -6,7 +6,7 @@ files {
     -- Main
     'edit_this.html',
     'loadscreen.css',
- -- 'bg1.png', Background Image
+ -- 'bg.png', Background Image
     
     -- Music
  -- 'music/music.mp3' Music File
